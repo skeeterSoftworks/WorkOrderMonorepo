558418a85261739dd76293a79bf4b28f921dc527
skeeterSoftworks <ghostskeeter@gmail.com>
2026-03-28 22:56:24 +0100

    styling fixes

--- packages/sf-common/src/modals/ConfirmationModal.tsx
+++ packages/sf-common/src/modals/ConfirmationModal.tsx
@@ -1,7 +1,10 @@
-import Container from '@mui/material/Container';
-import { Button, Dialog, DialogContent, DialogTitle, Grid } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Stack from '@mui/material/Stack';
 import { useTranslation } from 'react-i18next';
-import {centerHorizontal} from "../styling/Custom";
 import React from 'react';
 
 interface Props {
@@ -21,33 +24,75 @@ export function ConfirmationModal(props: Props) {
 
 
     return (
-        <Dialog open={props.open || false}>
-            <DialogTitle style={{ marginBottom: 20, color: "white", textAlign: "center", backgroundColor: "red" }}>
+        <Dialog
+            open={props.open || false}
+            onClose={handleModalClose}
+            fullWidth
+            maxWidth="sm"
+            scroll="paper"
+        >
+            <DialogTitle
+                sx={{
+                    color: 'common.white',
+                    textAlign: 'center',
+                    bgcolor: 'error.main',
+                    py: 1.5,
+                }}
+            >
                 {t("confirmSelectedAction")}
             </DialogTitle>
 
-            <DialogContent>
-                <Container style={{ ...centerHorizontal, marginTop: 20, width: 400 }}>
-                    <Grid item sm={6}>
-                        <Grid sx={{ height: 80 }}>
-                            {props.modalMessage}
-                        </Grid>
-                        <Grid container spacing={1} justifyContent="center">
-                            <Grid item>
-                                <Button variant="contained" type="button" color="success" onClick={() => { props.onConfirm && props.onConfirm() }}>
-                                {t("confirmAction")}
-                                </Button>
-                            </Grid>
-                            <Grid item>
+            <DialogContent
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    pt: 2,
+                    pb: 2,
+                    minWidth: 0,
+                    overflow: 'hidden',
+                }}
+            >
+                <DialogContentText
+                    component="div"
+                    sx={{
+                        m: 0,
+                        maxHeight: { xs: 'min(42vh, 280px)', sm: 'min(50vh, 360px)' },
+                        overflow: 'auto',
+                        whiteSpace: 'pre-wrap',
+                        wordBreak: 'break-word',
+                        pr: 0.5,
+                    }}
+                >
+                    {props.modalMessage}
+                </DialogContentText>
 
-                                <Button variant="contained" color="error" type="button" onClick={handleModalClose}>
-                                {t("cancelAction")}
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-
-                </Container>
+                <Stack
+                    direction={{ xs: 'column-reverse', sm: 'row' }}
+                    spacing={1}
+                    justifyContent="center"
+                    alignItems={{ xs: 'stretch', sm: 'center' }}
+                    sx={{ flexShrink: 0, pt: 0.5 }}
+                >
+                    <Button
+                        variant="contained"
+                        color="error"
+                        type="button"
+                        onClick={handleModalClose}
+                        sx={{ width: { xs: '100%', sm: 'auto' }, minWidth: { sm: 120 } }}
+                    >
+                        {t("cancelAction")}
+                    </Button>
+                    <Button
+                        variant="contained"
+                        type="button"
+                        color="success"
+                        onClick={() => { props.onConfirm && props.onConfirm() }}
+                        sx={{ width: { xs: '100%', sm: 'auto' }, minWidth: { sm: 120 } }}
+                    >
+                        {t("confirmAction")}
+                    </Button>
+                </Stack>
             </DialogContent>
         </Dialog>
     );
